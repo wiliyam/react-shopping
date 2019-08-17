df837f07b23217f544067236c144e426b08ee726
unknown <rajp@mobifyi.com>
2019-08-17 18:44:22 +0530

    fix icon

--- cloth-shop/src/component/cart-icon/cart-icon.js
+++ cloth-shop/src/component/cart-icon/cart-icon.js
@@ -4,15 +4,25 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
 import "./cart-icon.scss";
 
-const cartIcon = ({toggleCartHidden}) => (
+const cartIcon = ({ toggleCartHidden,itemsCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemsCount}</span>
   </div>
 );
 
-const mapDispatchToProp = dispatch =>( {
+const mapDispatchToProp = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null,mapDispatchToProp)(cartIcon);
+const mapStateToProps = state => ({
+  itemsCount: state.cart.cartItems.reduce(
+    (accuQuantity, cartItem) => accuQuantity + cartItem.quantity,
+    0
+  )
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProp
+)(cartIcon);
